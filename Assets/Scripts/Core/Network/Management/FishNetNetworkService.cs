0a81235028cb9f57ced0d8d4c0e3e984e2ceac2b
Artem <artemiys930519@yandex.ru>
2023-09-28 17:05:05 +0300

    update scene

--- Assets/Scripts/Core/Network/Management/FishNetNetworkService.cs
+++ Assets/Scripts/Core/Network/Management/FishNetNetworkService.cs
@@ -1,12 +1,13 @@
 ﻿using Core.Services;
 using Core.Services.InputService;
 using FishNet.Managing;
+using FishNet.Transporting;
 using UnityEngine;
 using Zenject;
 
 namespace Core.Network.Management
 {
-    public class FishNetNetworkManager : MonoBehaviour
+    public class FishNetNetworkService : MonoBehaviour
     {
         [SerializeField] private NetworkManager _networkManager;
         private INetworkWorldContext _networkWorldContext;
@@ -31,16 +32,18 @@ namespace Core.Network.Management
         {
             if (_networkManager == null)
                 return;
-
-            _networkManager.ServerManager.StartConnection(_networkWorldContext.Port);
+            _networkManager.TransportManager.Transport.SetPort(_networkWorldContext.Port);
+            _networkManager.ServerManager.StartConnection();
         }
 
         private void StartClient()
         {
+            _networkManager = FindObjectOfType<NetworkManager>();
             if (_networkManager == null)
                 return;
-
-            _networkManager.ClientManager.StartConnection(_networkWorldContext.IP,_networkWorldContext.Port);
+            _networkManager.TransportManager.Transport.SetPort(_networkWorldContext.Port);
+            _networkManager.TransportManager.Transport.SetClientAddress(_networkWorldContext.IP);
+            _networkManager.ClientManager.StartConnection();
         }
     }
 }
